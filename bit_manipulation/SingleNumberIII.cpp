2ed3419f6a8db158eb7a8829f956e5b369cfd813
Love Kumar <lovekumar714283@gmail.com>
2025-10-23 23:10:44 +0530

    SingleNumberIII.cpp

---
+++ bit_manipulation/SingleNumberIII.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<int> singleNumber(vector<int>& nums) {
+        long XORR = 0;
+        for(int i = 0;i<nums.size();i++) XORR ^= nums[i];
+        
+        int rightMost = (XORR & (XORR - 1)) ^ XORR;
+        int b1 = 0,b2 = 0;
+        for(int i = 0;i<nums.size();i++){
+            if(nums[i] & rightMost) b1 ^= nums[i];
+            else b2 ^= nums[i];
+        }
+        return {b1,b2};
+    }
+};
